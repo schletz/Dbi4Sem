3eead2711162453c2b6699aed85d5335976c61d6
Michael Schletz <schletz@gmail.com>
2020-03-03 12:41:28 +0100

    Commit 20200303

---
+++ SchulDbGenerator/README.md
@@ -0,0 +1,45 @@
+# SchulDb Generator
+
+Dieses Programm generiert - basierend auf den realen Untisdaten - Musterdaten für Schüler und
+Prüfungen. Es verwendet EF Core und schreibt die gesamte Datenbank neu.
+
+## Voraussetzung: .NET Core Version 3.1
+
+Dieses Programm verwendet .NET Core 3.1. Es kann daher unter Windows, Linux (64bit) und unter macOS
+ausgeführt werden. Prüfen Sie vorher in der Eingabeaufforderung (Konsole), ob
+Sie die entsprechende .NET Code Version installiert haben:
+
+```text
+dotnet --version
+```
+
+Liefert dieser Befehl einen Fehler oder ist die .NET Version kleiner als 3.1, laden Sie von der
+[dotnet Download Seite](https://dotnet.microsoft.com/download) die neueste SDK Version
+(nicht die Runtime) von .NET Core für Ihr Betriebssystem. Nach der Installation müssen Sie die
+Eingabeaufforderung beenden und neu öffnen.
+
+## Starten des Programmes
+
+Nun kann die Datenbank mit dem Generator erzeugt werden:
+
+```text
+cd SchulDbGenerator
+dotnet run -c Release
+```
+
+Das Programm fragt ab, welche Datenbank Sie anlegen möchten. Es kann eine SQLite Datei, eine SQL
+Server Datenbank (LocalDb) oder eine Oracle Datenbank angelegt werden.
+
+```text
+Welche Datenbank soll erstellt werden? [1]: SQLite (Default)   [2]: LocalDb   [3]: Oracle 1
+Dateiname? Hinweis: Relative Pfade (..) sind möglich. Default: Schule.db
+```
+
+- Um sich zur SQLite Datenbank zu verbinden, öffnen Sie DBeaver oder DataGrip und wählen eine SQLite
+  Verbindung.
+- Für den Zugriff auf die SQL Server Datenbank benötigen Sie das
+  [SQL Server Management Studio (SSMS)](https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15).
+  Dort können Sie sich zum Server *(LocalDb)\MSSQLLocalDB* (Windows Authentication) verbinden.
+- Für die Verwendung der Oracle Datenbank muss die Virtuelle Maschine vor der Generierung natürlich
+  gestartet werden. Nachdem die Datenbank erzeugt wurde, können Sie sich mit dem angezeigten
+  Benutzerdaten zu localhost, Service Name *orcl* verbinden.
---
+++ SchulDbGenerator/SchulDbGenerator/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchulDb.Model;
+using SchulDb.Untis;
+
+namespace SchulDbGenerator
+{
+    class Program
+    {
+        static DbContextOptions<SchuleContext> GetOptions()
+        {
+            var builder = new DbContextOptionsBuilder<SchuleContext>();
+            Console.Write("Welche Datenbank soll erstellt werden? [1]: SQLite (Default)   [2]: LocalDb   [3]: Oracle ");
+            string dbType = Console.ReadLine();
+            dbType = string.IsNullOrEmpty(dbType) ? "1" : dbType;
+
+            if (dbType == "1")
+            {
+                Console.Write("Dateiname? Hinweis: Relative Pfade (..) sind möglich. Default: Schule.db ");
+                string dbName = Console.ReadLine();
+                dbName = string.IsNullOrEmpty(dbName) ? "Schule.db" : dbName;
+                builder.UseSqlite($"DataSource={dbName}");
+            }
+            else if (dbType == "2")
+            {
+                Console.Write("Wie soll die Datenbank heißen? Default: Schule ");
+                string dbName = Console.ReadLine();
+                dbName = string.IsNullOrEmpty(dbName) ? "Schule" : dbName;
+                builder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;" +
+                                $"AttachDBFilename={System.Environment.CurrentDirectory}\\{dbName}.mdf;" +
+                                $"Database={dbName};" +
+                                $"Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+            else if (dbType == "3")
+            {
+                Console.Write("Wie soll der Benutzer heißen? Default: Schule ");
+                string dbName = Console.ReadLine();
+                dbName = string.IsNullOrEmpty(dbName) ? "Schule" : dbName;
+                builder.UseOracle($"User Id={dbName};Password=oracle;Data Source=localhost:1521/orcl");
+
+                var oracleSystemOptions = new DbContextOptionsBuilder<SchuleContext>()
+                    .UseOracle("User Id=System;Password=oracle;Data Source=localhost:1521/orcl")
+                    .Options;
+
+                Console.WriteLine($"Lege Benutzer {dbName} mit Passwort oracle an...");
+                using (SchuleContext db = new SchuleContext(oracleSystemOptions))
+                {
+                    try { db.Database.ExecuteSqlCommand("DROP USER " + dbName + " CASCADE"); }
+                    catch { }
+                    db.Database.ExecuteSqlCommand("CREATE USER " + dbName + " IDENTIFIED BY oracle");
+                    db.Database.ExecuteSqlCommand("GRANT CONNECT, RESOURCE, CREATE VIEW TO " + dbName);
+                    db.Database.ExecuteSqlCommand("GRANT UNLIMITED TABLESPACE TO " + dbName);
+                }
+            }
+            else
+            {
+                throw new SchulDb.SchulDbException("Ungültige Eingabe.");
+            }
+            return builder.Options;
+        }
+        static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                var options = GetOptions();
+                Untisdata data = await Untisdata.Load("Data", "2019-2020");
+                using (SchuleContext db = new SchuleContext(options))
+                {
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                    db.SeedDatabase(data, 2019);
+                }
+            }
+            catch (SchulDb.SchulDbException e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Console.Error.WriteLine(e?.InnerException?.Message);
+                Console.Error.WriteLine(e.StackTrace);
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Console.Error.WriteLine(e?.InnerException?.Message);
+                Console.Error.WriteLine(e.StackTrace);
+                return 2;
+            }
+            return 0;
+        }
+    }
+}
